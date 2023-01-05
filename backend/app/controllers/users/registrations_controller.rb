e82d829d6f3f4f0d92a5dd7deb94576fd98d0b76
aboongm <aboongm@yahoo.com>
2023-01-05 11:40:37 +0530

    Update registration_controller.rb with json responses for sign out functionality

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,62 +1,26 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+ respond_to :json
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+ private
 
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+ def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { 
+          code: 200, 
+          message: 'Sign up successfully!', 
+          data: resource },
+      }
+    else
+      render json: {
+        status: {
+          message: "User could not be created",
+          errors: resource.errors.full_messages
+        },
+        status: :unprocessable_entity
+      }
+    end    
+  end
 end
