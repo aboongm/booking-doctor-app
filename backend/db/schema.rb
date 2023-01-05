598e225a1de1e75b27ae7b14c35afbbcce39e2b5
aboongm <aboongm@yahoo.com>
2023-01-05 11:28:15 +0530

    Fix the ActionDispatch error in Postman signup

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_165940) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_054028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,12 +23,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_28_165940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
-    t.string "username"
-    t.string "role", default: "0"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
