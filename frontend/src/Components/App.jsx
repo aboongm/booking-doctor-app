ae84f9da243cfae904bd3b8297461351821fe0dd
aboongm <aboongm@yahoo.com>
2023-01-16 10:31:02 +0530

    Add bootstrap

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container py-4 px-3 mx-auto">
+      <h1 className="container text-primary">Hello World</h1>
+      <h1>React</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
