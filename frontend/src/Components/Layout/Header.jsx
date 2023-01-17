00931350a744d74f6017ee6e8bd658a82f7cc3fd
aboongm <aboongm@yahoo.com>
2023-01-17 10:22:27 +0530

    Add the header content

--- frontend/src/Components/Layout/Header.jsx
+++ frontend/src/Components/Layout/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  const content = (
+    <div className="container mx-auto border bg-gray d-flex align-center justify-content-center">
+      <div className="border flex-grow-1">logo</div>
+      <div className="d-flex align-center justify-content-between">
+        <div className="border d-flex ">
+          <p>What We Treat</p>
+          <p>Solutions</p>
+          <p>Medicare</p>
+          <p>MedicareAbout Us</p>
+        </div>
+        <div className="border">search</div>
+        <div className="border d-flex align-center">
+          <p>Sign in</p>
+          <p>Register</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  return content;
 };
 
 export default Header;
