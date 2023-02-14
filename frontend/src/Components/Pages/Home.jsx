9e7e4373bee2a398fefdcb4d441c345339a2bfdd
Lawrence C <lawrencecaudle870@gmail.com>
2023-02-13 21:39:58 -0500

    added columns and rows

--- frontend/src/Components/Pages/Home.jsx
+++ frontend/src/Components/Pages/Home.jsx
@@ -43,88 +43,96 @@ function Home() {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <p>
-                        Meet with an online therapist or psychiatrist when and
-                        where it works for you.
-                    </p>
+                <div className="row">
+                    <div className="col text-start">
+                        <p>
+                            Meet with an online therapist or psychiatrist when
+                            and where it works for you.
+                        </p>
+                    </div>
+                    <div className="col text-end">
+                        <Link to="/">
+                            <div>Online Therapy </div>
+                        </Link>
+                    </div>
                 </div>
-                <Link to="/">
-                    <div>Online Therapy </div>
-                </Link>
-            </div>
+                {/* WE'RE HERE FOR YOU */}
 
-            <div>
-                <div>
-                    <h2>We're Here For You</h2>
-                    <p>
-                        Meet with an urgent care doctor in minutes. Schedule a
-                        session with a{" "}
-                        <Link to="/">therapist or psychiatrist</Link> this week.
-                    </p>
-                </div>
-                <div>
-                    <div>
-                        <Link to="/">
-                            <h3>Cold & Flue</h3>
-                        </Link>
-                        <ul>
-                            <li>Cough</li>
-                            <li>Headaches</li>
-                            <li>Nausea & vomiting</li>
-                            <li>Fever</li>
-                        </ul>
+                <div className="row">
+                    <div className="col">
+                        <h2>We're Here For You</h2>
                     </div>
-                    <div>
-                        <h3>Skin Conditions</h3>
-                        <ul>
-                            <li>Acne</li>
-                            <li>Cellulitis</li>
-                            <li>Rashes</li>
-                            <li>Sunburn</li>
-                        </ul>
+                    <div className="col">
+                        <p>
+                            Meet with an urgent care doctor in minutes. Schedule
+                            a session with a{" "}
+                            <Link to="/">therapist or psychiatrist</Link> this
+                            week.
+                        </p>
                     </div>
-                    <div>
-                        <Link to="/">
-                            <h3>Mental Health</h3>
-                        </Link>
-                        <ul>
-                            <li>Anxiety</li>
-                            <li>Depression</li>
-                            <li>Stress</li>
-                            <li>PTSD</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Headaches</h3>
-                        <ul>
-                            <li>Migraines</li>
-                            <li>Sinus headaches</li>
-                            <li>Tension headaches</li>
-                            <li>Nausea</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Everyday Care</h3>
-                        <ul>
-                            <li>UTIs</li>
-                            <li>Fatigue</li>
-                            <li>IBS</li>
-                            <li>Joint pain</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <Link to="/">
-                            <h3>Allergies</h3>
-                        </Link>
-                        <ul>
-                            <li>Seasonal Allergies</li>
-                            <li>Epipen refills</li>
-                            <li>Allergic sinusitis</li>
-                            <li>Hives</li>
-                        </ul>
+
+                    <div className="row">
+                        <div className="col-md-4">
+                            <Link to="/">
+                                <h3>Cold & Flue</h3>
+                            </Link>
+                            <ul>
+                                <li>Cough</li>
+                                <li>Headaches</li>
+                                <li>Nausea & vomiting</li>
+                                <li>Fever</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-4">
+                            <h3>Skin Conditions</h3>
+                            <ul>
+                                <li>Acne</li>
+                                <li>Cellulitis</li>
+                                <li>Rashes</li>
+                                <li>Sunburn</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-4">
+                            <Link to="/">
+                                <h3>Mental Health</h3>
+                            </Link>
+                            <ul>
+                                <li>Anxiety</li>
+                                <li>Depression</li>
+                                <li>Stress</li>
+                                <li>PTSD</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-4">
+                            <h3>Headaches</h3>
+                            <ul>
+                                <li>Migraines</li>
+                                <li>Sinus headaches</li>
+                                <li>Tension headaches</li>
+                                <li>Nausea</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-4">
+                            <h3>Everyday Care</h3>
+                            <ul>
+                                <li>UTIs</li>
+                                <li>Fatigue</li>
+                                <li>IBS</li>
+                                <li>Joint pain</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-4">
+                            <Link to="/">
+                                <h3>Allergies</h3>
+                            </Link>
+                            <ul>
+                                <li>Seasonal Allergies</li>
+                                <li>Epipen refills</li>
+                                <li>Allergic sinusitis</li>
+                                <li>Hives</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
